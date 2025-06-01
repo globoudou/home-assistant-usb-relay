bb551c491dfe6ee150a70691512d4205eb4e3d5d
globoudou <43778062+globoudou@users.noreply.github.com>
2025-06-01 09:35:10 +0200

    Update config_flow.py

--- custom_components/usb_relay/config_flow.py
+++ custom_components/usb_relay/config_flow.py
@@ -1,19 +1,47 @@
-import voluptuous as vol
-
-from homeassistant import config_entries
-from .const import DOMAIN, DEFAULT_PORT, DEFAULT_RELAY_COUNT
-
 class USBRelayConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        errors = {}
-        if user_input is not None:
-            return self.async_create_entry(title="USB Relay", data=user_input)
+        ports = await self.hass.async_add_executor_job(detect_usb_relays)
+        if not ports:
+            return self.async_abort(reason="no_devices_found")
 
-        data_schema = vol.Schema({
-            vol.Required("port", default=DEFAULT_PORT): str,
-            vol.Optional("relay_count", default=DEFAULT_RELAY_COUNT): int,
+        self.detected_ports = {p[0]: p[1] for p in ports}  # stocke {port: relay_count}
+        return await self.async_step_select_port()
+
+    async def async_step_select_port(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            self.selected_port = user_input["port"]
+            self.relay_count = self.detected_ports.get(self.selected_port, 1)
+            return await self.async_step_confirm()
+
+        schema = vol.Schema({
+            vol.Required("port"): vol.In(list(self.detected_ports.keys()))
         })
 
-        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
+        return self.async_show_form(
+            step_id="select_port",
+            data_schema=schema,
+            errors=errors
+        )
+
+    async def async_step_confirm(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(
+                title=f"USB Relay ({self.selected_port})",
+                data={
+                    "port": self.selected_port,
+                    "relay_count": user_input["relay_count"]
+                }
+            )
+
+        schema = vol.Schema({
+            vol.Required("relay_count", default=self.relay_count): vol.All(int, vol.Range(min=1, max=8))
+        })
+
+        return self.async_show_form(
+            step_id="confirm",
+            data_schema=schema
+        )
