a5c49fda7f624b8fd53e9e005815e0af728f327f
globoudou <43778062+globoudou@users.noreply.github.com>
2025-06-12 19:01:09 +0200

    Update __init__.py

--- custom_components/usb_relay/__init__.py
+++ custom_components/usb_relay/__init__.py
@@ -20,13 +20,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, "switch")
+        await hass.config_entries.async_forward_entry_setups(entry, ["switch"])
     )
 
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(entry, "switch")
+    await hass.config_entries.async_unload_platforms(entry, ["switch"])
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
